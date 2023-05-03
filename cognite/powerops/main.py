88b97c669e179df9dff54a64f18deb2ea1040d4a
Juliamg <julia.graham@live.no>
2023-05-03 15:20:30 +0200

    pre-commit

--- cognite/powerops/main.py
+++ cognite/powerops/main.py
@@ -1,30 +1,25 @@
+from pathlib import Path
 from typing import Annotated
 
 import typer
 
-from pathlib import Path
-
 app = typer.Typer()
 
 
-@app.command("plan", help="Compare CDF resource creation between desired state (configuration files)"
-                          " and remote state (resources in CDF) and print differences")
+@app.command(
+    "plan",
+    help="Compare CDF resource creation between desired state (configuration files)"
+    " and remote state (resources in CDF) and print differences",
+)
 def plan(
     path: Annotated[Path, typer.Argument(help="Path to configuration files")],
 ):
-    typer.echo(
-        f"Running plan on configuration files located at {path}"
-    )
-
+    typer.echo(f"Running plan on configuration files located at {path}")
 
 
 @app.command("apply", help="Apply differences between desired and actual state. Writes desired state to CDF")
-def apply(
-    path: Annotated[Path, typer.Argument(help="Path to configuration files")]
-):
-    typer.echo(
-        f"Running apply on configuration files located at {path}"
-    )
+def apply(path: Annotated[Path, typer.Argument(help="Path to configuration files")]):
+    typer.echo(f"Running apply on configuration files located at {path}")
 
 
 def main():
