8b4b0100dc1f773cc64ff2ad84e19cccace9e9a2
Juliamg <julia.graham@live.no>
2023-05-08 13:16:42 +0200

    all code for running bootstrap with decoupled config files

--- cognite/powerops/main.py
+++ cognite/powerops/main.py
@@ -1,7 +1,13 @@
+from __future__ import annotations
+
+import os
 from pathlib import Path
 from typing import Annotated
 
 import typer
+
+from cognite.powerops.bootstrap import _create_cdf_resources, _load_config, _preview_resources_diff, _transform
+from cognite.powerops.logger import configure_debug_logging
 
 app = typer.Typer()
 
@@ -12,13 +18,56 @@ app = typer.Typer()
 )
 def plan(
     path: Annotated[Path, typer.Argument(help="Path to configuration files")],
+    market: Annotated[str, typer.Argument(help="Selected power market")],
 ):
     typer.echo(f"Running plan on configuration files located in {path}")
 
+    # Step 1 - configure
+    config = _load_config(path)
+    configure_debug_logging(config.constants.debug_level)
+
+    # Step 2 - transform from config to CDF resources and preview diffs
+    # 2.a - transform
+    bootstrap_resources = _transform(
+        config,
+        path,
+        market,
+    )
+
+    # 2.b - preview diff
+    _preview_resources_diff(
+        bootstrap_resources,
+        config.cdf.dict(),
+        config.constants.data_set_external_id,
+    )
+
 
 @app.command("apply", help="Apply the changes from the configuration files to the data model in CDF")
-def apply(path: Annotated[Path, typer.Argument(help="Path to configuration files")]):
+def apply(
+    path: Annotated[Path, typer.Argument(help="Path to configuration files")],
+    market: Annotated[str, typer.Argument(help="Selected power market")],
+):
     typer.echo(f"Running apply on configuration files located in {path}")
+
+    # Step 1 - configure
+    config = _load_config(path)
+    configure_debug_logging(config.constants.debug_level)
+
+    # Step 2 - transform from config to CDF resources and preview diffs
+    bootstrap_resources = _transform(
+        config,
+        path,
+        market,
+    )
+
+    # Step 3 - write bootstrap resources from diffs to CDF
+    _create_cdf_resources(
+        bootstrap_resources,
+        config.cdf.dict(),
+        config.constants.data_set_external_id,
+        config.constants.overwrite_data,
+        config.constants.skip_dm,
+    )
 
 
 def main():
---
+++ tests/test_bootstrap/approval_test/test_run.py
@@ -0,0 +1,44 @@
+import contextlib
+from pathlib import Path
+
+from cognite.client.testing import monkeypatch_cognite_client
+
+from cognite.powerops.main import apply
+
+from tests.test_bootstrap.approval_test.mock_resource_create_classes import (
+    MockAssetsCreate,
+    MockEventsCreate,
+    MockLabelsCreate,
+    MockRelationshipsCreate,
+    MockSequencesCreate,
+    MockTimeSeriesCreate,
+)
+
+DATA = Path(__file__).resolve().parent.parent / "data"
+
+
+def test_run(data_regression):
+    mock_resources = {
+        "assets": MockAssetsCreate(),
+        "sequences": MockSequencesCreate(),
+        "relationships": MockRelationshipsCreate(),
+        "time_series": MockTimeSeriesCreate(),
+        "labels": MockLabelsCreate(),
+        "events": MockEventsCreate(),
+    }
+
+    with monkeypatch_cognite_client() as client:
+        for resource_name, mock_resource in mock_resources.items():
+            api = getattr(client, resource_name)
+            api.create = mock_resource
+
+        apply(path=DATA / "demo", market="Dayahead")
+
+
+    dump = {resource_type: mock_resource.serialize() for resource_type, mock_resource in mock_resources.items()}
+
+    # for all the resources, sort the list of dicts by "external_id" in lowercase
+    for resource in dump.values():
+        with contextlib.suppress(KeyError):
+            resource.sort(key=lambda x: x["external_id"].lower())
+    data_regression.check(dump, basename="demo")
