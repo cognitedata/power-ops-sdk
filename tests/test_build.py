0d6e10f080f171c36359f71f57ab9acf72f5e263
anders-albert <anders0albert@gmail.com>
2023-05-12 16:41:20 +0200

    tests: Also check changelog

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,3 +1,5 @@
+import re
+
 import toml
 
 from cognite.powerops import version
@@ -7,10 +9,15 @@ from tests.constants import REPO_ROOT
 def test_matching_versions():
     with (REPO_ROOT / "pyproject.toml").open() as fh:
         pyproject_toml = toml.load(fh)
-
     version_in_pyproject_toml = pyproject_toml["tool"]["poetry"]["version"]
 
-    assert version.__version__ == version_in_pyproject_toml, (
+    changelog = (REPO_ROOT / "CHANGELOG.md").read_text()
+    if not (changelog_version_result := re.search(r"\[(\d+\.\d+\.\d+)\]", changelog)):
+        raise ValueError("Failed to obtain changelog version")
+    changelog_version = changelog_version_result.groups()[0]
+
+    assert version.__version__ == version_in_pyproject_toml == changelog_version, (
         f"Version in pyproject.toml ({version_in_pyproject_toml}) does not match version in "
-        f"cognite/powerops/version.py ({version.__version__})"
+        f"cognite/powerops/version.py ({version.__version__}) or in "
+        f"the CHANGELOG.md {changelog_version}"
     )
