8e9b4a99062ec83b760343db972633b4357c970a
anders-albert <anders0albert@gmail.com>
2023-05-15 10:33:50 +0200

    fix: added back dm client

--- cognite/powerops/client/powerops_client.py
+++ cognite/powerops/client/powerops_client.py
@@ -15,9 +15,8 @@ from cognite.powerops.client.asset_apis import (
     WatercourseAPI,
 )
 from cognite.powerops.client.config_client import ConfigurationClient
-
-# from cognite.powerops.client.dm.client import get_power_ops_dm_client
-# from cognite.powerops.client.dm_apis import CaseAPI, CommandsAPI, MappingAPI, ScenarioAPI, TransformationAPI
+from cognite.powerops.client.dm.client import get_power_ops_dm_client
+from cognite.powerops.client.dm_apis import CaseAPI, CommandsAPI, MappingAPI, ScenarioAPI, TransformationAPI
 from cognite.powerops.client.shop_api import ShopAPI
 
 
@@ -32,7 +31,7 @@ class ConfigurationsClient:
 
 class PowerOpsClient:
     def __init__(self, read_dataset: str, write_dataset: str, config: Optional[ClientConfig] = None):
-        # self.dm = get_power_ops_dm_client()
+        self.dm = get_power_ops_dm_client(config=config)
         self.core = CogniteClient(config)
 
         self.configurations = ConfigurationClient()
@@ -46,8 +45,8 @@ class PowerOpsClient:
         self.reservoirs = ReservoirsAPI(self.core, read_dataset, write_dataset)
         self.watercourses = WatercourseAPI(self.core, read_dataset, write_dataset)
 
-        # self.cases = CaseAPI(self.dm)
-        # self.commands = CommandsAPI(self.dm)
-        # self.scenarios = ScenarioAPI(self.dm)
-        # self.mappings = MappingAPI(self.dm)
-        # self.transformations = TransformationAPI(self.dm)
+        self.cases = CaseAPI(self.dm)
+        self.commands = CommandsAPI(self.dm)
+        self.scenarios = ScenarioAPI(self.dm)
+        self.mappings = MappingAPI(self.dm)
+        self.transformations = TransformationAPI(self.dm)
