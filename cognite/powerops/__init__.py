d02b398748b57df01a5e5076bc543a94b507f2af
Fran Hrženjak <fran.hrzenjak@gmail.com>
2023-05-22 18:37:52 +0200

    no message

--- cognite/powerops/__init__.py
+++ cognite/powerops/__init__.py
@@ -1,3 +1,5 @@
+from cognite.powerops.case import Case
+from cognite.powerops.client import PowerOpsClient
 from cognite.powerops.version import __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "Case", "PowerOpsClient"]
