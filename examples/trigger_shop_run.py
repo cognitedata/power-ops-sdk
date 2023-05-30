1d9e87c6a2f607384f50733d4219c2b3a5bf3492
Fran Hrženjak <fran.hrzenjak@gmail.com>
2023-05-30 20:53:27 +0200

    shop run trigger example

--- examples/trigger_shop_run.py
+++ examples/trigger_shop_run.py
@@ -3,8 +3,14 @@ from cognite.powerops.logger import configure_debug_logging
 
 configure_debug_logging("DEBUG")
 
-p = PowerOpsClient(read_dataset="uc:001:sandbox", write_dataset="uc:001:sandbox")
+p = PowerOpsClient()
 
-case = Case.load_yaml("/path/to/my/case.yaml")
+case = Case.load_yaml("/path/to/cogshop/test_cogshopsession/test_inputs/run.yaml")
+case.add_cut_file("/path/to/cogshop/test_cogshopsession/test_inputs/cut.txt")
+case.add_mapping_file(
+    "/path/to/cogshop/test_cogshopsession/test_inputs/reservoir_mapping.txt",
+    encoding="iso-8859-1",  # default: "utf-8
+)
+case.add_extra_file("/path/to/cogshop/test_cogshopsession/test_inputs/commands.yaml")
 
-p.shop.runs.trigger("Test_case_1", case)
+p.shop.runs.trigger(case)
