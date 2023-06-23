3517c5c92c9192cc458c1ddf20d1c0526459207d
Katrine Holm <katrine.holm@gmail.com>
2023-06-23 09:32:36 +0200

    update comment

--- cognite/powerops/client/data_classes/helpers.py
+++ cognite/powerops/client/data_classes/helpers.py
@@ -10,12 +10,11 @@ def format_deep_diff_path(path: str) -> str:
 
 
 def get_dict_dot_keys(data_dict: dict, dot_keys: str):
-    # FIXME?
-    # Workaround for the nested keys without dot get and
-    # the fact that the yaml parser parses numeric keys as numbers
-    # and datetime keys as strings
+    # Getting nested keys dot separated. Handles  that
+    # the yaml parser parses numeric keys as numbers
+    # and datetime keys as datetime.datetime(...)
     # Not a complete solution:
-    # Other datatypes might also have dots in their string representation
+    # Other datatypes need some handling might exist
     keys = [int(k) if k.isdigit() else k for k in dot_keys.split(".")]
     while "datetime" in keys:
         i = keys.index("datetime")
