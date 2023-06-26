977e92da1792205236caaf3cf46636307aee1e7e
Haukb <36861726+haukb@users.noreply.github.com>
2023-06-26 14:48:27 +0200

    fix: external_id being mixed up with name and thus not correctly used for relationships

--- cognite/powerops/utils/resource_generation.py
+++ cognite/powerops/utils/resource_generation.py
@@ -301,7 +301,6 @@ def add_generators_and_efficiency_curves(
         generator_name = replace_nordic_letters(generator_name)
 
         generator = Generator(
-            external_id=generator_name,
             name=generator_name,
             penstock=str(generator_information.get("penstock", "1")),
             p_min=generator_information.get("p_min", 0.0),
