6c6b7cafddacf6a6f4dd5c99a264e3e7607e782c
Fran Hrženjak <fran.hrzenjak@gmail.com>
2023-06-27 17:22:57 +0200

    remove toml, keep tomli

--- cognite/powerops/settings/__init__.py
+++ cognite/powerops/settings/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-import toml
+import tomli_w
 
 from cognite.powerops.settings.data_classes import Settings
 
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     # This is just a convenience for development, prints out all settings as TOML.
     # TOML has no support for `None`, filter it out:
-    _dump = toml.dumps(
+    _dump = tomli_w.dumps(
         {
             group_key: {key: val for key, val in group_val.items() if val is not None}
             for group_key, group_val in settings.dict().items()
--- tests/test_build.py
+++ tests/test_build.py
@@ -1,14 +1,14 @@
 import re
 
-import toml
+import tomli
 
 from cognite.powerops import version
 from tests.constants import REPO_ROOT
 
 
 def test_matching_versions():
-    with (REPO_ROOT / "pyproject.toml").open() as fh:
-        pyproject_toml = toml.load(fh)
+    with (REPO_ROOT / "pyproject.toml").open("rb") as fh:
+        pyproject_toml = tomli.load(fh)
     version_in_pyproject_toml = pyproject_toml["tool"]["poetry"]["version"]
 
     changelog = (REPO_ROOT / "CHANGELOG.md").read_text()
