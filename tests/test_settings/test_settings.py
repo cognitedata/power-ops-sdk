5a5fb3cce1cdcd6557fea3709ef41207e6d05b1c
Fran Hrženjak <fran.hrzenjak@gmail.com>
2023-06-30 14:27:45 +0200

    removing unused fields from settings model

--- tests/test_settings/test_settings.py
+++ tests/test_settings/test_settings.py
@@ -6,10 +6,7 @@ def test_settings_empty(no_files, settings):
             "cdf_cluster": None,
             "client_id": None,
             "client_secret": None,
-            "data_model": None,
             "project": None,
-            "schema_version": None,
-            "space": None,
             "tenant_id": None,
         },
         "powerops": {
@@ -26,19 +23,19 @@ def test_settings_empty(no_files, settings):
 
 def test_settings_from_env(with_env_vars, no_files, settings):
     assert settings.cognite.project == "BAR"
-    assert settings.cognite.space == "mockspace"
+    assert settings.cognite.client_id == "mockclient"
     assert settings.cognite.cdf_cluster is None
 
 
 def test_settings_from_files(with_files, settings):
     assert settings.cognite.project == "1"
-    assert settings.cognite.space == "22"
+    assert settings.cognite.client_id == "22"
     assert settings.powerops.write_dataset == "333"
     assert settings.cognite.cdf_cluster is None
 
 
 def test_settings_merge(with_env_vars, with_files, settings):
     assert settings.cognite.project == "BAR"  # env overrides files
-    assert settings.cognite.space == "mockspace"  # env overrides files
+    assert settings.cognite.client_id == "mockclient"  # env overrides files
     assert settings.powerops.write_dataset == "333"
     assert settings.cognite.cdf_cluster is None
