a677d312effa51fcb3fb0dc61afa98bedfe2dd0a
anders-albert <anders0albert@gmail.com>
2023-07-10 08:37:52 +0200

    refactor: first draft of CogShop model

--- cognite/powerops/resync/models/cogshop.py
+++ cognite/powerops/resync/models/cogshop.py
@@ -1,5 +1,9 @@
+from cognite.powerops.clients.cogshop.data_classes import CaseApply, ModelTemplateApply, ScenarioApply
+
 from ._base import DataModel
 
 
 class CogShopModel(DataModel):
-    ...
+    model_templates: list[ModelTemplateApply]
+    ScenarioApply: list[ScenarioApply]
+    cases: list[CaseApply]
