cb10da89cca3c6ed6cbedba72680692ff7866f59
anders-albert <anders0albert@gmail.com>
2023-07-13 10:56:36 +0200

    refactor: updated generate client scripts

--- scripts/generate_data_model_clients.py
+++ scripts/generate_data_model_clients.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from cognite.pygen import generate_sdk
 
+from cognite.powerops._models import MODEL_BY_NAME
 from cognite.powerops.utils.cdf import get_cognite_client
 
 REPO_ROOT = Path(__file__).parent.parent
@@ -10,32 +11,15 @@ REPO_ROOT = Path(__file__).parent.parent
 def main():
     client = get_cognite_client()
     top_level_base = "cognite.powerops.clients"
-    generate_sdk(
-        client,
-        ("cogShop", "CogShop", "1"),
-        top_level_package=f"{top_level_base}.cogshop",
-        client_name="CogShopClient",
-        output_dir=REPO_ROOT,
-        logger=print,
-    )
-
-    generate_sdk(
-        client,
-        ("power-ops", "Core", "1"),
-        top_level_package=f"{top_level_base}.core",
-        client_name="CoreClient",
-        output_dir=REPO_ROOT,
-        logger=print,
-    )
-
-    generate_sdk(
-        client,
-        ("power-ops", "MarketConfiguration", "1"),
-        top_level_package=f"{top_level_base}.market_configuration",
-        client_name="MarketConfigClient",
-        output_dir=REPO_ROOT,
-        logger=print,
-    )
+    for model in MODEL_BY_NAME.values():
+        generate_sdk(
+            client,
+            model.id_,
+            top_level_package=f"{top_level_base}.cogshop",
+            client_name=model.client_name,
+            output_dir=REPO_ROOT,
+            logger=print,
+        )
 
 
 if __name__ == "__main__":
