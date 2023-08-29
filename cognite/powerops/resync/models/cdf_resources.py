6b2e5efd400ac9df994ced62426d8f1af7655500
anders-albert <anders0albert@gmail.com>
2023-08-29 07:44:41 +0200

    feat: Added dumping of content hash

--- cognite/powerops/resync/models/cdf_resources.py
+++ cognite/powerops/resync/models/cdf_resources.py
@@ -1,57 +1,48 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from hashlib import sha256
 from typing import Any, ClassVar, Optional
 
 import pandas as pd
-from cognite.client import CogniteClient
 from cognite.client.data_classes import FileMetadata, Sequence
 from cognite.client.data_classes._base import CogniteResource
-from cognite.client.exceptions import CogniteNotFoundError
+
 from pydantic import BaseModel, ConfigDict, model_serializer, model_validator
 from typing_extensions import Self
 
 from cognite.powerops.resync.utils.serializer import remove_read_only_fields
 
 
-class _CDFResource(BaseModel, ABC):
+class CDFResource(BaseModel, ABC):
+    content_key_hash: ClassVar[str] = "sha256_hash"
     model_config: ClassVar[ConfigDict] = ConfigDict(arbitrary_types_allowed=True)
 
     @model_serializer
     def ser_model(self) -> dict[str, Any]:
         return self.dump(camel_case=False)
 
-    @abstractmethod
-    def _dump(self, camel_case: bool = False) -> dict[str, Any]:
-        ...
-
     def dump(self, camel_case: bool = False) -> dict[str, Any]:
         return remove_read_only_fields(self._dump(camel_case))
+
+    @abstractmethod
+    def _dump(self, camel_case: bool = False) -> dict[str, Any]:
+        raise NotImplementedError()
 
     @property
     def cdf_resource(self) -> CogniteResource:
         raise NotImplementedError
 
-    @classmethod
-    @abstractmethod
-    def from_cdf(
-        cls,
-        client: CogniteClient,
-        resource_ext_id: str,
-        fetch_content: bool = False,
-    ) -> _CDFResource:
-        ...
-
-
-class CDFSequence(_CDFResource):
-    sequence: Sequence
-    content: Optional[pd.DataFrame] = None
-
     def __repr__(self) -> str:
-        return f"CDFSequence(external_id={self.external_id})"
+        return f"{type(self).__name}({self.external_id=})"
 
     def __str__(self) -> str:
         return repr(self)
+
+
+class CDFSequence(CDFResource):
+    sequence: Sequence
+    content: Optional[pd.DataFrame] = None
 
     @property
     def cdf_resource(self) -> Sequence:
@@ -67,11 +58,6 @@ class CDFSequence(_CDFResource):
     def external_id(self):
         return self.sequence.external_id
 
-    def _set_data_set_id(self, data_set_id: int):
-        self.sequence.data_set_id = data_set_id
-
-    data_set_id = property(None, fset=_set_data_set_id)
-
     def dump(self, camel_case: bool = False) -> dict[str, Any]:
         output = super().dump(camel_case)
         if (columns := output.get("columns")) and isinstance(columns, list):
@@ -80,6 +66,18 @@ class CDFSequence(_CDFResource):
         return output
 
     def _dump(self, camel_case: bool = False) -> dict[str, Any]:
+        if self.content is None and (self.sequence.metadata or {}).get(self.content_key_hash) is None:
+            sha256_hash = "Missing Content"
+        elif self.content is not None:
+            sha256_hash = sha256(
+                pd.util.hash_pandas_object(self.content, index=True).values, usedforsecurity=False
+            ).hexdigest()
+        else:
+            sha256_hash = self.sequence.metadata[self.content_key_hash]
+        if self.sequence.metadata:
+            self.sequence.metadata[self.content_key_hash] = sha256_hash
+        else:
+            self.sequence.metadata = {self.content_key_hash: sha256_hash}
         return self.sequence.dump(camel_case=camel_case)
 
     @classmethod
@@ -88,37 +86,10 @@ class CDFSequence(_CDFResource):
             sequence=Sequence._load(data),
         )
 
-    @classmethod
-    def from_cdf(
-        cls,
-        client: CogniteClient,
-        resource_ext_id: str,
-        fetch_content: bool = False,
-    ) -> CDFSequence:
-        sequence = client.sequences.retrieve(external_id=resource_ext_id)
-        if sequence is None:
-            raise CogniteNotFoundError([resource_ext_id])
-        if fetch_content:
-            # limit defaults to 100, might not be an issue
-            content = client.sequences.data.retrieve_dataframe(
-                external_id=resource_ext_id,
-                start=0,
-                end=None,
-            )
-        else:
-            content = None
-        return cls(sequence=sequence, content=content)
 
-
-class CDFFile(_CDFResource):
+class CDFFile(CDFResource):
     meta: FileMetadata
     content: Optional[bytes] = None
-
-    def __repr__(self) -> str:
-        return f"CDFFile(external_id={self.external_id})"
-
-    def __str__(self) -> str:
-        return self.__repr__()
 
     @property
     def cdf_resource(self) -> FileMetadata:
@@ -135,6 +106,17 @@ class CDFFile(_CDFResource):
         return self.meta.external_id
 
     def _dump(self, camel_case: bool = False) -> dict[str, Any]:
+        if self.content is None and (self.meta.metadata or {}).get(self.content_key_hash) is None:
+            sha256_hash = "Missing Content"
+        elif self.content is not None:
+            # The replacement is used to ensure that the hash is the same on Windows and Linux
+            sha256_hash = sha256(self.content.replace(b"\r\n", b"\n"), usedforsecurity=False).hexdigest()
+        else:
+            sha256_hash = self.meta.metadata[self.content_key_hash]
+        if self.meta.metadata:
+            self.meta.metadata[self.content_key_hash] = sha256_hash
+        else:
+            self.meta.metadata = {self.content_key_hash: sha256_hash}
         return self.meta.dump(camel_case=camel_case)
 
     @classmethod
@@ -142,19 +124,3 @@ class CDFFile(_CDFResource):
         return cls(
             meta=FileMetadata._load(data),
         )
-
-    @classmethod
-    def from_cdf(
-        cls,
-        client: CogniteClient,
-        resource_ext_id: str,
-        fetch_content: bool = False,
-    ) -> CDFFile:
-        meta = client.files.retrieve(external_id=resource_ext_id)
-        if meta is None:
-            raise CogniteNotFoundError([resource_ext_id])
-        if fetch_content:
-            content = client.files.download_bytes(external_id=resource_ext_id)
-        else:
-            content = None
-        return cls(meta=meta, content=content)
