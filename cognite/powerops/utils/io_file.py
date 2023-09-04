f16e81c38ad01f18c00f028bed3b983157ef7725
anders-albert <anders0albert@gmail.com>
2023-09-04 14:25:00 +0200

    feat: Script for upgrading customers SDKs version

--- cognite/powerops/utils/io_file.py
+++ cognite/powerops/utils/io_file.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from pathlib import Path
 from typing import Any
+import tomli_w
 
 try:
     import tomllib
@@ -10,3 +11,7 @@ except ModuleNotFoundError:
 
 def read_toml_file(toml_file: Path | str) -> dict[str, Any]:
     return tomllib.loads(Path(toml_file).read_text())
+
+
+def write_toml_file(toml_file: Path | str, data: dict[str, Any]) -> None:
+    Path(toml_file).write_text(tomli_w.dumps(data))
