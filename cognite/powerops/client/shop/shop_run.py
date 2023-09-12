2a84a76dcc73b446c9d379415f3d45717c472218
anders-albert <anders0albert@gmail.com>
2023-09-12 12:59:08 +0200

    feat: Implemented list SHOP runs

--- cognite/powerops/client/shop/shop_run.py
+++ cognite/powerops/client/shop/shop_run.py
@@ -1,19 +1,73 @@
+from collections import UserList
+from dataclasses import dataclass
 from datetime import datetime
+from typing import ClassVar
 
-from cognite.client.data_classes._base import CogniteResource, CogniteResourceList
+import pandas as pd
+from cognite.client.data_classes.events import Event, EventList
+from cognite.client.utils import ms_to_datetime
+from typing_extensions import Self
 
 
-@classmethod
-class SHOPRun(CogniteResource):
+class ShopRunEvent:
+    event_type: ClassVar[str] = "POWEROPS_PROCESS_REQUESTED"
+    event_subtype: ClassVar[str] = "POWEROPS_SHOP_RUN"
+    watercourse: ClassVar[str] = "shop:watercourse"
+
+
+@dataclass
+class SHOPRun:
+    """
+    This represents a single SHOP run.
+
+    A SHOP run is represented by an event in CDF. This class is a wrapper around the event.
+
+    Args:
+        external_id: The external ID of the SHOP run. This matches the external ID of the event in CDF.
+        watercourse: The watercourse of the SHOP run.
+        start: The start time of the SHOP run.
+        end: The end time of the SHOP run.
+    """
+
     external_id: str
     watercourse: str
     start: datetime
     end: datetime
 
+    @classmethod
+    def load(cls, event: Event) -> Self:
+        """
+        Load a SHOP run from an event.
 
-class SHOPRunList(CogniteResourceList[SHOPRun]):
+        Args:
+            event: The event to load from.
+
+        Returns:
+
+        """
+        metadata = event.metadata or {}
+        return cls(
+            external_id=event.external_id,
+            watercourse=metadata.get(ShopRunEvent.watercourse, ""),
+            start=ms_to_datetime(event.start_time),
+            end=ms_to_datetime(event.end_time),
+        )
+
+    def dump(self) -> dict[str, str]:
+        return {"external_id": self.external_id, "watercourse": self.watercourse, "start": self.start, "end": self.end}
+
+
+class SHOPRunList(UserList):
     """
     This represents a list of SHOP runs.
     """
 
-    _RESOURCE = SHOPRun
+    @classmethod
+    def load(cls, events: EventList) -> Self:
+        return cls([SHOPRun.load(event) for event in events])
+
+    def to_pandas(self) -> pd.DataFrame:
+        return pd.DataFrame([run.dump() for run in self.data])
+
+    def _repr_html_(self) -> str:
+        return self.to_pandas()._repr_html_()
--- cognite/powerops/client/shop/shop_run_api.py
+++ cognite/powerops/client/shop/shop_run_api.py
@@ -1,19 +1,17 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
-from typing import ClassVar
+from typing import overload
 
 from cognite.client import CogniteClient
+from cognite.client.data_classes import filters
 
-from .shop_run import SHOPRun, SHOPRunList
+from .shop_run import SHOPRun, ShopRunEvent, SHOPRunList
 
 DEFAULT_READ_LIMIT = 25
 
 
 class SHOPRunAPI:
-    _cdf_event_type: ClassVar[str] = "POWEROPS_PROCESS_REQUESTED"
-    _cdf_event_subtype: ClassVar[str] = "POWEROPS_SHOP_RUN"
-
     def __init__(self, client: CogniteClient):
         self._cdf = client
 
@@ -21,7 +19,36 @@ class SHOPRunAPI:
         raise NotImplementedError()
 
     def list(self, watercourse: str | list[str] | None = None, limit: int = DEFAULT_READ_LIMIT) -> SHOPRunList:
-        raise NotImplementedError()
+        """List the filtered SHOP runs.
+
+        Args:
+            watercourse: The watercourse to filter on.
+            limit: The maximum number of SHOP runs to return. D
+
+        Returns:
+            A list of SHOP runs.
+        """
+
+        is_type = filters.Equals("type", ShopRunEvent.event_type)
+        is_subtype = filters.Equals("subtype", ShopRunEvent.event_subtype)
+        extra_filters = []
+        if watercourse:
+            watercourses = watercourse if isinstance(watercourse, list) else [watercourse]
+            is_watercourse = filters.ContainsAny(["metadata", ShopRunEvent.watercourse], [watercourses])
+            extra_filters.append(is_watercourse)
+
+        selected = filters.And(is_type, is_subtype, *extra_filters)
+        events = self._cdf.events.filter(selected, limit=limit)
+
+        return SHOPRunList.load(events)
+
+    @overload
+    def retrieve(self, external_id: str) -> SHOPRun:
+        ...
+
+    @overload
+    def retrieve(self, external_id: Sequence[str]) -> SHOPRunList:
+        ...
 
     def retrieve(self, external_id: str | Sequence[str]) -> SHOPRun | SHOPRunList:
         raise NotImplementedError()
