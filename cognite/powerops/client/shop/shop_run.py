090d6fe68a12bf5ab44a91700cd3e74bee8c2ff3
anders-albert <anders0albert@gmail.com>
2023-09-14 10:43:29 +0200

    refactor: Pretty run in notebook

--- cognite/powerops/client/shop/shop_run.py
+++ cognite/powerops/client/shop/shop_run.py
@@ -228,8 +228,8 @@ class SHOPRun:
         )
         self._run_event_types |= {cast(Event, rel.target).type for rel in relationships}
 
-    def _repr_html_(self) -> pd.DataFrame:
-        return pd.Series(self.dump()).to_frame().rename(columns={0: "Value"})
+    def _repr_html_(self) -> str:
+        return pd.Series(self.dump()).to_frame().rename(columns={0: "Value"})._repr_html_()
 
 
 class SHOPRunList(UserList):
