15504edd01520cd136c33ee3be817a3c894d2c53
Julia Graham <47179573+Juliamg@users.noreply.github.com>
2023-09-18 15:08:55 +0200

    Update cognite/powerops/resync/config/market/dayahead.py
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- cognite/powerops/resync/config/market/dayahead.py
+++ cognite/powerops/resync/config/market/dayahead.py
@@ -39,13 +39,10 @@ class BidProcessConfig(Configuration):
         return {"operations": json.loads(value)} if isinstance(value, str) else value
 
     @field_validator("price_scenarios", mode="after")
-    def literal_eval_and_uniqueness(cls, value):
-        literal_price_scenarios = [{"id": id_} for id_ in value] if isinstance(value, str) else value
-        seen = set()
-        unique_price_scenarios = []
-        for price_scenario in literal_price_scenarios:
-            if price_scenario.id in seen:
-                raise ValueError("Duplicate price scenarios for bidprocess was found.")
-            seen.add(price_scenario.id)
-            unique_price_scenarios.append(price_scenario)
-        return unique_price_scenarios
+    def ensure_no_duplicates(cls, value: list[PriceScenarioID], info: FieldValidationInfo):
+        scenario_ids = Counter(scenario.id for scenario in value)
+        duplicates = [scenario_id for scenario_id, count in scenario_ids.items() if count > 1]
+        if duplicates:
+            bidprocess_name = info.data.get("name", "unknown")
+            raise ValueError(f"Duplicate price scenarios for bidprocess {bidprocess_name} was found: {list(duplicates)}")
+        return value
