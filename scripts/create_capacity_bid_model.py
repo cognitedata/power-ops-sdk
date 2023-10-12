3f5ba1603a7997beb2bf11a686c4e0a354bd0070
anders-albert <anders0albert@gmail.com>
2023-10-12 16:35:13 +0200

    style: sorting imports

--- scripts/create_capacity_bid_model.py
+++ scripts/create_capacity_bid_model.py
@@ -3,12 +3,15 @@ Depends on cognite-neat, needs to be installed manually 'pip install cognite-nea
 It cannot be set in the pyproject.toml because it requires Python 3.10, pyproject toml
 seems to ignore 'cognite-neat = {version = "^0.30.0", python = ">3.9"}.
 """
-from cognite.neat.rules import importer
-from cognite.client.data_classes.data_modeling import ContainerApplyList, ViewApplyList
-from cognite.neat.rules.exporter.rules2dms import DataModel
-from cognite.powerops.resync.models.v2.dms import CapacitySourceModel, CapacityModel
-import yaml
 from pathlib import Path
+
+import yaml
+from cognite.client.data_classes.data_modeling import ContainerApplyList, ViewApplyList
+from cognite.neat.rules import importer
+from cognite.neat.rules.exporter.rules2dms import DataModel
+
+from cognite.powerops.resync.models.v2.dms import CapacitySourceModel, CapacityModel
+
 
 ROOT = Path(__file__).parent.parent
 CAPACITY_MARKET_JSON = ROOT / "customers" / "data" / "970_0ca0c919-046a-4940-b191-f5cc4d0e6513.json"
