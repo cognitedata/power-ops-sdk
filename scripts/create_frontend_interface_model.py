f39053151299224cef2604309f7f8fdfa15b5fbe
Fran Hrženjak <fran.hrzenjak@gmail.com>
2023-11-27 14:30:42 +0100

    yaml schema, cdf-tk

---
+++ scripts/create_frontend_interface_model.py
@@ -0,0 +1,31 @@
+"""
+Helper script that builds and deploys the frontend interface model to CDF.
+
+It populates the environment variables required by cdf-tk from the settings
+files (settings.toml and .secrets.toml), so no need for a local .env file.
+"""
+import os
+import subprocess
+
+from pathlib import Path
+
+from cognite.powerops.utils.cdf import Settings
+
+ROOT = Path(__file__).parent.parent
+
+
+def main():
+    settings = Settings()
+    env = os.environ.copy()
+    env["CDF_CLUSTER"] = settings.cognite.cdf_cluster
+    env["CDF_PROJECT"] = settings.cognite.project
+    env["IDP_CLIENT_ID"] = settings.cognite.client_id
+    env["IDP_CLIENT_SECRET"] = settings.cognite.client_secret
+    env["IDP_TOKEN_URL"] = f"https://login.microsoftonline.com/{settings.cognite.tenant_id}/oauth2/v2.0/token"
+
+    subprocess.check_call(["cdf-tk", "build", ".", "--clean", "--env=local"], env=env, cwd=ROOT / "interface_frontend")
+    subprocess.check_call(["cdf-tk", "deploy", "--env=local"], env=env, cwd=ROOT / "interface_frontend")
+
+
+if __name__ == "__main__":
+    main()
