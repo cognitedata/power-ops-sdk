01014ee5f8a5347b83cf81934802fb07e530dd24
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-12-01 09:22:28 +0100

    Update resync config (#251)
    * refactor: Missing configs
    
    * refactor: minor changes to script
    
    * tests: updated test data

--- scripts/run_apply.py
+++ scripts/run_apply.py
@@ -1,11 +1,12 @@
 from cognite.powerops.cli import apply
+from cognite.powerops.resync import MODELS_BY_NAME
 from tests.constants import ReSync, REPO_ROOT
 from cognite.powerops.utils.serialization import chdir
 
 
 def main():
     with chdir(REPO_ROOT):
-        apply(ReSync.demo, "Dayahead", ["ProductionModelDM"], auto_yes=True, format="markdown", verbose=True)
+        apply(ReSync.demo, "Dayahead", sorted(MODELS_BY_NAME), auto_yes=False, format="markdown", verbose=True)
 
 
 if __name__ == "__main__":
