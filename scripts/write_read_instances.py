b36b44179c0bc89fc4dae628d90029cf2b723157
anders-albert <anders0albert@gmail.com>
2024-02-17 09:43:00 +0100

    build: update pygen

--- scripts/write_read_instances.py
+++ scripts/write_read_instances.py
@@ -1,6 +1,7 @@
 from cognite.client.exceptions import CogniteAPIError
 from cognite.pygen.utils import MockGenerator
 from cognite.client import data_modeling as dm
+from cognite.client.data_classes import filters
 import os
 from pathlib import Path
 import time
@@ -12,6 +13,7 @@ from cognite.powerops.utils.cdf import get_cognite_client
 from cognite.powerops.utils.serialization import chdir
 
 REPO_ROOT = Path(__file__).parent.parent
+INSTANCE_SPACE = "sp_powerops_instance"
 
 
 def main():
@@ -22,7 +24,6 @@ def main():
     print(f"Connected to {client.config.project}")
 
     data_model_ids = [dm.DataModelId("sp_powerops_models", "all_PowerOps", "1")]
-    instance_space = "sp_powerops_instance"
     node_count = 5
     for data_model_id in data_model_ids:
         data_models = client.data_modeling.data_models.retrieve(data_model_id, inline_views=True)
@@ -32,7 +33,7 @@ def main():
         data_model = data_models.latest_version()
         leaf_views_by_parent = MockGenerator._to_leaf_children_by_parent(data_model.views)
 
-        mock_generator = MockGenerator(data_model.views, instance_space=instance_space, seed=42, skip_interfaces=True)
+        mock_generator = MockGenerator(data_model.views, instance_space=INSTANCE_SPACE, seed=42, skip_interfaces=True)
         mock_data = mock_generator.generate_mock_data(node_count=node_count)
         print(f"Generated {len(mock_data.nodes)} nodes for {len(data_model.views)} views.")
 
@@ -41,6 +42,7 @@ def main():
         print(f"Deployed {len(mock_data.nodes)} nodes to {len(data_model.views)} views.")
 
         # Check Read.
+        correct_count = 0
         for view in data_model.views:
             view_id = view.as_id()
             try:
@@ -55,8 +57,10 @@ def main():
             if len(nodes) != expected_nodes:
                 print(f"Print unexpected number of nodes for {view_id}: {len(nodes)} instead of {expected_nodes}.")
             else:
+                correct_count += 1
                 print(f"Read {len(nodes)} nodes for {view_id} as expected.")
 
+        print(f"Read {correct_count} views out of {len(data_model.views)} views as expected.")
         mock_data.clean(client)
 
 
@@ -65,21 +69,34 @@ def clean_instances():
         os.environ["SETTINGS_FILES"] = "settings.toml;.secrets.toml"
         client = get_cognite_client()
     t0 = time.perf_counter()
-    for edges in client.data_modeling.instances(
-        instance_type="edge", space="sp_powerops_instance", limit=-1, chunk_size=100
-    ):
+    for edges in client.data_modeling.instances(instance_type="edge", space=INSTANCE_SPACE, limit=-1, chunk_size=100):
         deleted = client.data_modeling.instances.delete(edges.as_ids())
         print(f"Deleted {len(deleted.edges)} edges. Elapsed time {time.perf_counter() - t0:.2f} seconds")
     else:
         print("Done deleting edges")
 
-    for nodes in client.data_modeling.instances(
-        instance_type="node", space="sp_powerops_instance", limit=-1, chunk_size=500
-    ):
+    for nodes in client.data_modeling.instances(instance_type="node", space=INSTANCE_SPACE, limit=-1, chunk_size=500):
         deleted = client.data_modeling.instances.delete(nodes.as_ids())
         print(f"Deleted {len(deleted.nodes)} nodes. Elapsed time {time.perf_counter() - t0:.2f} seconds")
     else:
         print("Done deleting nodes")
+
+    is_mock_generator = filters.Equals(["metadata", "source"], "PygenMockGenerator")
+
+    timeseries = client.time_series.filter(limit=-1, filter=is_mock_generator)
+    if timeseries:
+        client.time_series.delete(id=[ts.id for ts in timeseries])
+        print(f"Deleted {len(timeseries)} timeseries. Elapsed time {time.perf_counter() - t0:.2f} seconds")
+
+    sequences = client.sequences.filter(limit=-1, filter=is_mock_generator)
+    if sequences:
+        client.sequences.delete(id=[seq.id for seq in sequences])
+        print(f"Deleted {len(sequences)} sequences. Elapsed time {time.perf_counter() - t0:.2f} seconds")
+
+    files = client.files.list(limit=-1, metadata={"source": "PygenMockGenerator"})
+    if files:
+        client.files.delete(id=[file.id for file in files])
+        print(f"Deleted {len(files)} files. Elapsed time {time.perf_counter() - t0:.2f} seconds")
 
 
 if __name__ == "__main__":
