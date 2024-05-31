51dc06344bd8872d5e82eca674ca4fccd322f6b5
cpnat <cpnattrass@googlemail.com>
2024-05-31 13:51:25 +0200

    WIP

---
+++ defs.py
@@ -0,0 +1,12 @@
+import os
+from cognite.powerops import PowerOpsClient
+from cognite.powerops.client.shop.shop_case import SHOPCase
+
+os.environ["SETTINGS_FILES"] = "settings.toml; secrets.toml"
+
+powerops = PowerOpsClient.from_settings()
+
+def shop_run_func(case: SHOPCase):
+    print('submitting case')
+    powerops.shop.trigger_single_casefile(case)
+    print('case submitted')
