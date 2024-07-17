d168e608f3b8b1bea8e85fb584b5cb289894695b
Emilie Dahl <emilie.dahl@cognite.com>
2024-07-17 17:00:12 +0200

    v2: added changes to the script according to comments on the PR

--- scripts/add_ts_data_bidmatrixinfo.py
+++ scripts/add_ts_data_bidmatrixinfo.py
@@ -1,53 +1,59 @@
 ###this script adds time series data to the bid matrix information objects in the powerops client
 
 from cognite.client.data_classes import TimeSeries
+from cognite.client.exceptions import CogniteNotFoundError
 from cognite.powerops import PowerOpsClient
 import random
 import datetime
 
-
 power = PowerOpsClient.from_settings()
 cdf = power.cdf
+
+exid_hourly = "emilies_points"  # exid_hourly = "wim_ts_hourly"
+exid_empty = "wim_ts_empty"
+tsList = [exid_hourly, exid_empty]
 
 start_time = int((datetime.datetime.now() - datetime.timedelta(days=365)).timestamp() * 1000)
 end_time = int(datetime.datetime.now().timestamp() * 1000)
 
-exid_hourly = "wim_ts_hourly"
-exid_empty = "wim_ts_empty"
+# Checking if the time series already exists and adding them to the list existing_time_series if they do
+# ignore_unknown_ids=True will ignore any external_ids that don't exist in CDF. Remove this if you want to raise an error if the external_id doesn't exist
+try:
+    existing_time_series = cdf.time_series.retrieve_multiple(external_ids=tsList, ignore_unknown_ids=True)
+    existing_time_series_ids = []
+    for ts in existing_time_series:
+        existing_time_series_ids.append(ts.external_id)
 
-# Checking if the time series already exists
-existing_time_series = cdf.time_series.retrieve_multiple(external_ids=[exid_hourly, exid_empty])
-
-existing_time_series_ids = []
-for ts in existing_time_series:
-    existing_time_series_ids.append(ts.external_id)
+except CogniteNotFoundError as e:
+    print(f"Error: {e}")
+# print(existing_time_series)
 
 
 # Adding the time series to a list, if they don't exist
 time_series_to_create = []
-
 if exid_hourly not in existing_time_series_ids:
-    time_series_to_create.append(TimeSeries(name="wim_ts_hourly", external_id=exid_hourly))
+    time_series_to_create.append(TimeSeries(name="emilies_points", external_id=exid_hourly))
+
+if exid_empty not in existing_time_series_ids:
+    time_series_to_create.append(TimeSeries(external_id=exid_empty))
+
+# Creating the time series that don't exist
+if time_series_to_create:
+    cdf.time_series.create(time_series_to_create)
+
+# Insert data points for the hourly ts
+if exid_hourly not in existing_time_series_ids:
     data_points = [
         {"timestamp": start_time + i * 3600 * 1000, "value": random.random()}
         for i in range(int((end_time - start_time) / (3600 * 1000)))
     ]
     cdf.time_series.data.insert(datapoints=data_points, external_id=exid_hourly)
 
-if exid_empty not in existing_time_series_ids:
-    time_series_to_create.append(TimeSeries(external_id=exid_empty))
 
-# Creating the time series if the list has any elements
-if time_series_to_create:
-    cdf.time_series.create(time_series_to_create)
-
-tsList = [exid_hourly, exid_empty]
-
-
-def get_random_timeseries():
+def get_random_timeseries(list_of_time_series):
     # Choosing a random number between 1 and length of tsList
-    num_to_choose = random.randint(1, len(tsList))
-    return random.sample(tsList, num_to_choose)
+    num_to_choose = random.randint(1, len(list_of_time_series))
+    return random.sample(list_of_time_series, num_to_choose)
 
 
 # print(get_random_timeseries())
@@ -63,12 +69,10 @@ for matrix in power.v1.day_ahead_bid.bid_matrix_information.list():
     mat = matrix.as_write()
     # Keeping the exid from before .as_write, as there's a bug in the .as_write() that was overwriting the external_id:
     mat.external_id = exid  # This line can be removed once the .as_write() bug is fixed
-    mat.linked_time_series = get_random_timeseries()
+    mat.linked_time_series = get_random_timeseries(tsList)
     upsertlist.append(mat)
+    # print(mat)
 
-# Printing the matrices:
-for matrix in upsertlist:
-    print(matrix)
 
 # Printing the results:
 res = power.v1.upsert(upsertlist, replace=True)
