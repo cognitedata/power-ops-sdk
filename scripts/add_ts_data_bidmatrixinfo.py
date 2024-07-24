f92d39448bbd18e24bda62dd16aaa4e55d84e872
Emilie Dahl <emilie.dahl@cognite.com>
2024-07-24 12:26:11 +0200

    updated script according to suggestions

--- scripts/add_ts_data_bidmatrixinfo.py
+++ scripts/add_ts_data_bidmatrixinfo.py
@@ -1,18 +1,24 @@
 ###this script adds time series data to the bid matrix information objects in the powerops client
 
-from typing import Union
-
 from cognite.client.data_classes import TimeSeries, TimeSeriesList
 from cognite.client.exceptions import CogniteNotFoundError
 from cognite.client import CogniteClient
 from cognite.powerops import PowerOpsClient
-
 from cognite.powerops.client._generated.v1.data_classes import BidMatrixInformationWrite
+
+from typing import Union
 import random
 import arrow
 
 
-def create_time_series_objects():
+def create_time_series_objects() -> list[TimeSeries]:
+    """
+    Create a list of time series objects with predefined metadata.
+
+    Returns:
+        list[TimeSeries]: A list of TimeSeries objects.
+    """
+
     ts_list = [
         TimeSeries(external_id="emilie_ts_onhour", name="emilie_ts_onhour", metadata={"data_type": "on_hour"}),
         TimeSeries(external_id="emilie_ts_empty", name="emilie_ts_empty", metadata={"data_type": "none"}),
@@ -21,12 +27,22 @@ def create_time_series_objects():
     return ts_list
 
 
-# Function to get existing time series from CDF
 def get_existing_time_series(
     client: CogniteClient, ts_list: list[TimeSeries]
 ) -> tuple[list[TimeSeries], list[TimeSeries]]:
-    want_external_ids = {ts.external_id: ts for ts in ts_list}
+    """
+    Fetches time series that exist in CDF based on a list of input time series to search for.
 
+    Args:
+        client (CogniteClient): An authenticated CDF client.
+        ts_list (list[TimeSeries]): A list of time series to search for.
+
+    Returns:
+        existing_ts (list[TimeSeries]): A list of the time series that already exist in CDF based on the input list.
+        missing_ts (list[TimeSeries]): A list of the time series that do not exist in CDF.
+    """
+
+    want_external_ids = {ts.external_id: ts for ts in ts_list}
     existing_ts_from_cdf = client.time_series.retrieve_multiple(
         external_ids=list(want_external_ids.keys()), ignore_unknown_ids=True
     )
@@ -38,8 +54,19 @@ def get_existing_time_series(
     return existing_ts, missing_ts
 
 
-# Function to create or update time series in CDF
 def create_update_time_series(client: CogniteClient, to_be_updated: list[TimeSeries], missing_ts: list[TimeSeries]):
+    """
+    Creates or updates time series in CDF
+
+    Args:
+        client (CogniteClient): An authenticated CDF client.
+        to_be_updated: list[TimeSeries]: A time series list holding the time series that should be updated.
+        missing_ts: list[TimeSeries]: A time series list to be created from the missing time series (not in CDF)
+
+    Returns:
+        A list of the created and updated time series as list[TimeSeries]:
+    """
+
     created = []
     if missing_ts:
         created = client.time_series.create(missing_ts)
@@ -55,14 +82,28 @@ def create_update_time_series(client: CogniteClient, to_be_updated: list[TimeSer
     return created + updated
 
 
-# Function to clean data points for a list of time series
 def clean_data_points(client: CogniteClient, ts_list: list[TimeSeries]):
+    """
+    Cleans data points for a list of time series
+
+    Args:
+        client (CogniteClient): An authenticated CDF client.
+        ts_list (list[TimeSeries]): A list of time series whose data points need to be cleaned/deleted.
+    """
+
     ranges = [{"external_id": ts.external_id, "start": 0, "end": "now"} for ts in ts_list]
     client.time_series.data.delete_ranges(ranges)
 
 
-# Function to insert data points into according to the type of time series (definined in the metadata)
 def insert_data_points(client: CogniteClient, ts_list: list[TimeSeries]):
+    """
+    Inserts data points into time series according to the type of time series (definined in the metadata)
+
+    Args:
+        client (CogniteClient): An authenticated CDF client.
+        ts_list (list[TimeSeries]): A list of time series to insert data points into.
+    """
+
     now = arrow.now().floor("hour")
     start_time = now.shift(days=-365).timestamp() * 1000
     end_time = now.timestamp() * 1000
@@ -85,21 +126,39 @@ def insert_data_points(client: CogniteClient, ts_list: list[TimeSeries]):
             ]
             dps_to_insert.append({"external_id": ts.external_id, "datapoints": data_points})
 
-        elif data_type == "none":
-            print("bar")
+        # elif data_type == "none":
+        #     print("something")
 
     # Insert the generated data points into CDF
     client.time_series.data.insert_multiple(dps_to_insert)
 
 
-# Function to get a random number of items from a list
 def get_random_item(list_of_items: list) -> Union[list, None]:  # Alternatively: Optional[list]
+    """
+    Gets a random number of items from a list
+
+    Args:
+        list_of_items (list): A list of items to choose from.
+
+    Returns:
+        Union[list, None]: A random number of items from the list, or None if the list is empty.
+    """
+
     num_to_choose = random.randint(0, len(list_of_items))
     return random.sample(list_of_items, num_to_choose)
 
 
-# Function to link time series to bid matrix information objects
 def link_ts_to_bid_matrix_info(power: PowerOpsClient, ts_list: list[TimeSeries]) -> list[BidMatrixInformationWrite]:
+    """
+    Links time series to bid matrix information objects.
+
+    Args:
+        power (PowerOpsClient): An authenticated PowerOps client.
+        ts_list (list[TimeSeries]): A list of time series to be linked.
+
+    Returns:
+        list[BidMatrixInformationWrite]: A list of bid matrix information objects with linked time series.
+    """
     upsert_list = []
 
     for matrix in power.v1.day_ahead_bid.bid_matrix_information.list(limit=None):
@@ -113,8 +172,14 @@ def link_ts_to_bid_matrix_info(power: PowerOpsClient, ts_list: list[TimeSeries])
     return upsert_list
 
 
-# Main function to execute the above functions and upsert the bid matrix information objects with the linked time series (either updated or created)
 def main():
+    """
+    Main function to execute the script.
+
+    This function initializes the PowerOps and CDF clients, creates or updates time series,
+    cleans existing data points, inserts new data points, and links the time series to bid matrix information objects using upsert.
+    """
+
     power_client = PowerOpsClient.from_settings()
     cdf_client = power_client.cdf
 
@@ -139,15 +204,6 @@ def main():
 
     # Upsert the bid matrix information objects with the linked time series:
     power_client.v1.upsert(upsert_list, replace=True)
-
-
-# Printing the results with the upsert (comment out the upsert above first):
-# res = power.v1.upsert(upsertlist, replace=True)
-# for r in res:
-#    print(r)
-
-# check one of the matrices:
-# print(power.v1.day_ahead_bid.bid_matrix_information.retrieve(external_id="total_bid_matrix_calculation_generated_8e412afb-a56c-4ca4-ac53-3c2b3fec37f7_4cf4_2023-5-1_8bd1_total_bid_matrix_6e32"))
 
 
 if __name__ == "__main__":
