c7c0019dc40fbe6f4107736bb146402a6bf74912
Katrine Holm <katrine.holm@gmail.com>
2024-10-15 13:39:10 +0200

    never skip warning

--- cognite/powerops/utils/deprecation.py
+++ cognite/powerops/utils/deprecation.py
@@ -2,22 +2,17 @@ import warnings
 from typing import TypeVar
 
 T = TypeVar("T")
-_WARNED_CLASSES: set[str] = set()
 
 
 def deprecated_class(cls: type[T]) -> type[T]:
     """Decorator to mark a class as deprecated, showing a message only once."""
 
-    # Intercept method calls
     def new_getattribute(self, name):  # type: ignore[no-untyped-def]
-        class_name = cls.__name__
-        if class_name not in _WARNED_CLASSES:
-            warnings.warn(
-                f"\nWarning: Class `{cls.__name__}` is deprecated and will be removed.",
-                DeprecationWarning,
-                stacklevel=2,
-            )
-            _WARNED_CLASSES.add(class_name)
+        warnings.warn(
+            f"\nWarning: Class `{cls.__name__}` is deprecated and will be removed.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         return super(cls, self).__getattribute__(name)
 
     cls.__getattribute__ = new_getattribute  # type: ignore[method-assign]
