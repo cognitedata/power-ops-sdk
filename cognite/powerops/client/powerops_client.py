c83df5f3f9e2b3226b5cde1c690a5b416801ec35
Nina Odegard <nina.odegard@cognite.com>
2024-10-25 15:35:53 +0200

    wip

--- cognite/powerops/client/powerops_client.py
+++ cognite/powerops/client/powerops_client.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
+from string import Template
 
-from cognite.client import ClientConfig, CogniteClient
+import yaml
+import logging
+from dotenv import load_dotenv
+
+from cognite.client import CogniteClient, global_config
 
 from cognite.powerops.client.shop.cogshop_api import CogShopAPI
 from cognite.powerops.utils.cdf import Settings, get_client_config
@@ -22,14 +28,15 @@ from .shop.shop_run_api import SHOPRunAPI
 # max_domain = max_total (255) - uuid (32) + separator (1)  noqa: ERA001
 _MAX_DOMAIN_LENGTH = 233
 
+logger = logging.getLogger(__name__)
 
 class PowerOpsClient:
     def __init__(
         self,
-        read_dataset: str,
-        write_dataset: str,
-        config: ClientConfig,
-        monitor_dataset: str | None = None,
+        # read_dataset: str,
+        # write_dataset: str,
+        client: CogniteClient,
+        # monitor_dataset: str | None = None,
     ):
         self.cdf = CogniteClient(config)
         self.datasets = DataSetsAPI(self.cdf, read_dataset, write_dataset, monitor_dataset)
@@ -76,83 +83,46 @@ class PowerOpsClient:
         return f"PowerOpsClient with {', '.join(map(lambda a: '.' + a, self._apis().keys()))} APIs"
 
     @classmethod
-    def from_client(
+    def from_config(
         cls,
-        client: CogniteClient,
-        *,
-        read_dataset: str | None = None,
-        write_dataset: str | None = None,
-        monitor_dataset: str | None = None,
+        config_path: Path | str,
     ) -> PowerOpsClient:
         """
-        Create a PowerOpsClient from a CogniteClient object.
-
-        This uses default values for the read and write data sets, cogshop version and monitor data set.
+        Create a PowerOpsClient from a configuration file.
 
         Args:
-            client: The CogniteClient object.
-            read_dataset: externalId of read data set. Optional, by default loaded from the settings object.
-            write_dataset: externalId of write data set. Optional, by default loaded from the settings object.
-            monitor_dataset: externalId of monitor data set. Optional, by default loaded from the settings object.
+            config_path: The path to a yaml configuration file.
 
         Returns:
             A PowerOpsClient object.
         """
-        return cls.from_settings(
-            config=client.config,
-            read_dataset=read_dataset,
-            write_dataset=write_dataset,
-            monitor_dataset=monitor_dataset,
+
+        if isinstance(config_path, str):
+            config_path = Path(config_path)
+
+        # Read in yaml file and fetch all environment variables referenced in the file
+        env_sub_template = Template(config_path.read_text())
+        all_identifiers = env_sub_template.get_identifiers()
+
+        load_dotenv()
+        env_dict = dict(os.environ)
+
+        missing_env_vars = set(all_identifiers) - set(env_dict.keys())
+        if missing_env_vars:
+            raise ValueError(f"Missing environment variables: {missing_env_vars}")
+
+        # Substitute environment variables in the file string
+        file_env_parsed = env_sub_template.substitute(env_dict)
+
+        # Load yaml file string into a dictionary to parse global and client configurations
+        cognite_config = yaml.safe_load(file_env_parsed)
+
+        # If you want to set a global configuration it must be done before creating the client
+        global_config.apply_settings(cognite_config["global"])
+        client = CogniteClient.load(cognite_config["client"])
+
+        power_ops_config = cognite_config["powerops"]
+
+        return cls(
+            client=client,
         )
-
-    @classmethod
-    def from_settings(
-        cls,
-        settings: Settings | None = None,
-        *,
-        config: ClientConfig | None = None,
-        read_dataset: str | None = None,
-        write_dataset: str | None = None,
-        monitor_dataset: str | None = None,
-    ) -> PowerOpsClient:
-        """
-        Create a PowerOpsClient from a Settings object.
-
-
-        Args:
-            settings: The settings object. If no settings object is provided, the settings object is loaded from
-                the environment. When loading the Settings object from the environment,
-                the environment variable `SETTINGS_FILES` is used to
-                specify which files to load. The default value is `settings.toml;.secrets.toml`.
-            config: The client config object. Optional, by default it is loaded from the settings object.
-            read_dataset: externalId of read data set. Optional, by default loaded from the settings object.
-            write_dataset: externalId of write data set. Optional, by default loaded from the settings object.
-            monitor_dataset: externalId of monitor data set. Optional, by default loaded from the settings object.
-
-        Returns:
-            A PowerOpsClient object.
-        """
-        settings = settings or Settings()
-
-        client_config = config if config is not None else get_client_config(settings.cognite)
-
-        return PowerOpsClient(
-            config=client_config,
-            read_dataset=(read_dataset if read_dataset is not None else settings.powerops.read_dataset),
-            write_dataset=(write_dataset if write_dataset is not None else settings.powerops.write_dataset),
-            monitor_dataset=(monitor_dataset if monitor_dataset is not None else settings.powerops.monitor_dataset),
-        )
-
-    @classmethod
-    def from_toml(cls, toml_file: str | Path) -> PowerOpsClient:
-        """
-        Create a PowerOpsClient from a TOML file.
-
-        Args:
-            toml_file: Path to the TOML file.
-
-        Returns:
-            A PowerOpsClient object.
-        """
-        content = read_toml_file(toml_file)
-        return cls.from_settings(settings=Settings.model_validate(content))
