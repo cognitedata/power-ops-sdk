ccd9d847f84797acf0578934bec8ed44d6513202
Nina Ødegård <nina.odegard@cognite.com>
2024-10-29 16:21:38 +0100

    Update tests/test_unit/test_client/test_powerops_client.py
    Co-authored-by: Erik Lien Johnsen <erik.lien.johnsen@gmail.com>

--- tests/test_unit/test_client/test_powerops_client.py
+++ tests/test_unit/test_client/test_powerops_client.py
@@ -2,7 +2,7 @@ from cognite.powerops.client.powerops_client import PowerOpsClient
 from tests.constants import REPO_ROOT
 
 
-def test_from_yaml():
+def test_from_config():
     config_path = REPO_ROOT / "power_ops_config.yaml"
 
     po_client = PowerOpsClient.from_config(config_path)
