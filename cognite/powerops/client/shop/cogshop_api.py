327752cb2164ec41648edf61bc6455e3c7f5fb2d
Katrine Holm <katrine.holm@gmail.com>
2024-11-01 11:31:02 +0100

    add prepare_shop_case

--- cognite/powerops/client/shop/cogshop_api.py
+++ cognite/powerops/client/shop/cogshop_api.py
@@ -1,7 +1,15 @@
+import datetime
 from urllib.parse import urlparse
 
 import requests
 from cognite.client import CogniteClient
+
+from cognite.powerops.client._generated.v1.data_classes import (
+    ShopCaseWrite,
+    ShopFileWrite,
+    ShopModelWrite,
+    ShopScenarioWrite,
+)
 
 
 class CogShopAPI:
@@ -35,6 +43,70 @@ class CogShopAPI:
             auth=auth,
         )
         response.raise_for_status()
+
+    def prepare_shop_case(
+        self,
+        files: list[tuple[str, str, bool, str]],
+        shop_version: str,
+        start_time: datetime.datetime,
+        end_time: datetime.datetime,
+        model_name: str,
+        scenario_name: str,
+        model_external_id: str | None = None,
+        scenario_external_id: str | None = None,
+        case_external_id: str | None = None,
+    ) -> ShopCaseWrite:
+        """
+        Prepare a SHOP case that can be written to cdf.
+        External ids must be unique. If they are not provided, they will be generated.
+        Args:
+            files: List of 4-tuples and every item is expected.
+                    It is assumed that the order of the list if the order the files should be loaded into SHOP.
+                Format:
+                `file_reference`: external if of file in CDF.
+                `file_name`: Name of the file.
+                `is_ascii`: Whether the file is in ASCII format.
+                `labels`: Labels to be added to the fil, use "" if no labels
+
+            start_time: Start of time range SHOP is optimized over
+            end_time: End of time range SHOP is optimized over
+            shop_version: Version of SHOP to use (e.g. '16.0.2'or '15.7.0.0'), required.
+
+            scenario_name: Name of the scenario. Required, does not have to be unique
+            model_name: Name of the model. Required, does not have to be unique
+            scenario_external_id: External ID of the scenario. Optional, must be unique
+            model_external_id: External ID of the model. Optional, must be unique
+            case_external_id: External ID of the SHOP case. Optional, must be unique
+        Returns:
+            ShopCaseWrite: A SHOP case that can be written to CDF
+        """
+        model_write = ShopModelWrite(name=model_name, external_id=model_external_id, shop_version=shop_version)
+
+        scenario_write = ShopScenarioWrite(
+            name=scenario_name,
+            external_id=scenario_external_id,
+            model=model_write,
+        )
+
+        shop_files_write = [
+            ShopFileWrite(
+                name=file_name,
+                fileReference=file_reference,
+                isAscii=is_ascii,
+                labels=labels,
+                order=i + 1,  # Order is 1-indexed
+            )
+            for i, (file_reference, file_name, is_ascii, labels) in enumerate(files)
+        ]
+
+        case_write = ShopCaseWrite(
+            external_id=case_external_id,
+            start_time=start_time,
+            end_time=end_time,
+            scenario=scenario_write,
+            shop_files=shop_files_write,
+        )
+        return case_write
 
     def list_shop_versions(self) -> list[str]:
         """List the available version of SHOP remotely  in CDF.
