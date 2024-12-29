aca318617897a9cc4a6a411cb4f62a81dd1f5116
Nina Ødegård <nina.odegard@cognite.com>
2024-12-29 13:54:19 -0600

    feat: resync purge (#479)
    * feat: resync purge nodes and edges
    
    * feat: print out purge output
    
    * feat: configure purge excludes
    
    * Update RESYNC.md
    
    Co-authored-by: Erik Lien Johnsen <erik.lien.johnsen@gmail.com>
    
    ---------
    
    Co-authored-by: Erik Lien Johnsen <erik.lien.johnsen@gmail.com>

--- cognite/powerops/resync/main.py
+++ cognite/powerops/resync/main.py
@@ -5,9 +5,10 @@ from pathlib import Path
 
 from cognite.powerops.client import PowerOpsClient
 from cognite.powerops.resync.config_to_fdm import ResyncImporter
+from cognite.powerops.resync.purge import ResyncPurge
 from cognite.powerops.resync.utils import get_data_model_write_classes
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("resync")
 
 
 def pre_build(client_configuration: Path, configuration: Path) -> None:
@@ -17,6 +18,7 @@ def pre_build(client_configuration: Path, configuration: Path) -> None:
     objects from the configuration. The plan is then printed to the console.
 
     Args:
+        client_configuration: Path to the CDF client configuration file.
         configuration: Path to the resync configuration file.
     """
     client = PowerOpsClient.from_config(client_configuration)
@@ -29,3 +31,19 @@ def pre_build(client_configuration: Path, configuration: Path) -> None:
     logger.info(resync_data_model_objects)
     logger.info(f"Generated {len(resync_data_model_objects)} node objects")
     logger.info(f"External IDs: {external_ids}")
+
+
+def purge(client_configuration: Path, configuration: Path, dry_run: bool = False, verbose: bool = False) -> None:
+    """Checks which nodes & edges are not included in the toolkit files to be deleted.
+
+    Args:
+        client_configuration: Path to the CDF client configuration file.
+        configuration: Path to the resync configuration file.
+        dry_run: Whether or not to actually delete the nodes/edges.
+        verbose: Print out detailed information about nodes & edges to delete.
+    """
+
+    client = PowerOpsClient.from_config(client_configuration)
+
+    resync_purge = ResyncPurge.from_yaml(configuration, client.cdf, dry_run, verbose)
+    resync_purge.purge()
